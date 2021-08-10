6af27d1111e321bc818330085fda277603e2e6f8
patlim8 <patsap8@gmail.com>
2021-08-10 10:33:48 +0700

    8/10/2021

--- pages/api/line.js
+++ pages/api/line.js
@@ -313,25 +313,25 @@ export default function test(req, res) {
             };
             qr.decode(image.bitmap);
 
-            fetch(process.env.API+'/coupon/used', {
-                method: 'PUT', // *GET, POST, PUT, DELETE, etc.
-                mode: 'cors', // no-cors, *cors, same-origin
-                cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-                credentials: 'same-origin', // include, *same-origin, omit
-                headers: {
-                  'Content-Type': 'application/json'
-                  // 'Content-Type': 'application/x-www-form-urlencoded',
-                },
-                redirect: 'follow', // manual, *follow, error
-                referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-                body: JSON.stringify(couponInfo) // body data type must match "Content-Type" header
-              })
-                .then(response => response.json())
-                .then(data => {
-                  console.log(data);
-                  // alert("Update:\nResponse from server " + data.message)
-                  // alert("Update", data._id)
-                });
+            // fetch(process.env.API+'/coupon/used', {
+            //     method: 'PUT', // *GET, POST, PUT, DELETE, etc.
+            //     mode: 'cors', // no-cors, *cors, same-origin
+            //     cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+            //     credentials: 'same-origin', // include, *same-origin, omit
+            //     headers: {
+            //       'Content-Type': 'application/json'
+            //       // 'Content-Type': 'application/x-www-form-urlencoded',
+            //     },
+            //     redirect: 'follow', // manual, *follow, error
+            //     referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+            //     body: JSON.stringify(couponInfo) // body data type must match "Content-Type" header
+            //   })
+            //     .then(response => response.json())
+            //     .then(data => {
+            //       console.log(data);
+            //       // alert("Update:\nResponse from server " + data.message)
+            //       // alert("Update", data._id)
+            //     });
           });
         });
       });
