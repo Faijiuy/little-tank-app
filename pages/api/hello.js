d93107e63cde0dbbc6bd766a58a931c1ba595db5
patlim8 <patsap8@gmail.com>
2021-09-03 16:33:10 +0700

    line

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -3,15 +3,18 @@ import { format } from 'date-fns'
 
 export default function handler() {
 
-  var date = format(new Date(), "dd/MM"); // months start 0
-  let year = new Date().getFullYear()
-  let test = date + "/" + (new Date().getFullYear() + 543)
+  // var date = format(new Date(), "dd/MM"); // months start 0
+  // let year = new Date().getFullYear()
+  // let test = date + "/" + (new Date().getFullYear() + 543)
   
-  if(new Date().getFullYear() >= 2564){
-    console.log(year + 543)
-  }else{
-    console.log(test);
+  // if(new Date().getFullYear() >= 2564){
+  //   console.log(year + 543)
+  // }else{
+  //   console.log(test);
 
-  }
+  // }
+
+  console.log(typeof new Date())
+
 }
 
