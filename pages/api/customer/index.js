662786a7204d4da010ddb85efc7386fa46ea326c
patlim8 <patsap8@gmail.com>
2021-09-05 15:08:00 +0700

    test 8

--- pages/api/customer/index.js
+++ pages/api/customer/index.js
@@ -1,19 +1,15 @@
-// // import { ObjectID } from "mongodb";
-// // import { ObjectId } from 'bson';
-// // import { ObjectId} from "bson";
-// import { ObjectId } from "bson";
-// import { connectToDatabase } from "../../../util/mongodb";
+import { connectToDatabase } from "../../../util/mongodb";
 
-// export default async (req, res) => {
-//   // console.log("item API", req);
-//   if (req.method === "GET") {
-//     const { db } = await connectToDatabase();
-//     const customer = await db
-//       .collection("customer")
-//       .find({})
-//       .sort({})
-//       .limit(0)
-//       .toArray();
-//     res.json(customer);
-//   }
-// };
+export default async (req, res) => {
+  // console.log("item API", req);
+  if (req.method === "GET") {
+    const { db } = await connectToDatabase();
+    const customer = await db
+      .collection("customer")
+      .find({})
+      .sort({})
+      .limit(0)
+      .toArray();
+    res.json(customer);
+  }
+};
