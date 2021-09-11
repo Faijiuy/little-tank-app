ef7e336f0e3140bddfbc52f8864e9cdc02c16ee3
patlim8 <patsap8@gmail.com>
2021-09-11 11:13:48 +0700

    improve color

--- components/couponMgt/generateCoupon.js
+++ components/couponMgt/generateCoupon.js
@@ -1,57 +1,20 @@
 import React, { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 
-import { withStyles } from "@material-ui/core/styles";
 import { makeStyles } from "@material-ui/core/styles";
-import MuiAccordion from "@material-ui/core/Accordion";
-import MuiAccordionSummary from "@material-ui/core/AccordionSummary";
-import MuiAccordionDetails from "@material-ui/core/AccordionDetails";
-import Typography from "@material-ui/core/Typography";
+
 import GridContainer from "components/Grid/GridContainer.js";
 import InputLabel from "@material-ui/core/InputLabel";
 import MenuItem from "@material-ui/core/MenuItem";
 import FormControl from "@material-ui/core/FormControl";
 import Select from "@material-ui/core/Select";
 import { DataGrid } from "@material-ui/data-grid";
-import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemIcon from "@material-ui/core/ListItemIcon";
-import ListItemText from "@material-ui/core/ListItemText";
-import Checkbox from "@material-ui/core/Checkbox";
+
 import Button from "components/CustomButtons/Button.js";
 
 
-import Grid from "@material-ui/core/Grid";
-import Card from "@material-ui/core/Card";
-import CardHeader from "@material-ui/core/CardHeader";
-import Divider from "@material-ui/core/Divider";
-
 import { format } from 'date-fns'
-import DateFnsUtils from "@date-io/date-fns";
-import {
-  MuiPickersUtilsProvider,
-  KeyboardDatePicker,
-} from "@material-ui/pickers";
 
-
-const styles = {
-  cardCategoryWhite: {
-    color: "rgba(255,255,255,.62)",
-    margin: "0",
-    fontSize: "14px",
-    marginTop: "0",
-    marginBottom: "0",
-  },
-  cardTitleWhite: {
-    color: "#FFFFFF",
-    marginTop: "0px",
-    minHeight: "auto",
-    fontWeight: "300",
-    fontFamily: "'Roboto', 'Helvetica', 'Arial', sans-serif",
-    marginBottom: "3px",
-    textDecoration: "none",
-  },
-};
 
 const useStyles2 = makeStyles((theme) => ({
   formControl: {
@@ -76,6 +39,7 @@ const useStyles4 = makeStyles({
       backgroundColor: 'rgba(255, 7, 0, 0.55)',
     },
   },
+ 
 });
 
 
@@ -86,7 +50,6 @@ function GenerateCoupon({ customers, coupons }) {
   const classes4 = useStyles4();
 
   const [company, setCompany] = useState("");
-  const [type, setType] = useState();
 
   const [date, setDate] = useState();
 
@@ -135,11 +98,11 @@ function GenerateCoupon({ customers, coupons }) {
     setRows(newArr);
   };
 
-  // const handleDelete = (params) => {
-  //   let newRows = rows.filter(row => row.id !== params.row.id)
+  const handleDelete = (params) => {
+    let newRows = rows.filter(row => row.id !== params.row.id)
     
-  //   setRows(newRows)
-  // }
+    setRows(newRows)
+  }
 
   const columns = [
     {
@@ -149,6 +112,18 @@ function GenerateCoupon({ customers, coupons }) {
       type: "number",
       width: 120,
       editable: true,
+      renderCell: (cellValue) => {
+        return (
+          <div
+            style={
+              cellValue.value < 500 ? 
+              {color: "red"} : null
+            }
+          >
+            {cellValue.value}
+          </div>
+        )
+      }  
     },
     {
       field: "qty",
@@ -157,36 +132,50 @@ function GenerateCoupon({ customers, coupons }) {
       type: "number",
       width: 120,
       editable: true,
+      renderCell: (cellValue) => {
+        return (
+          <div
+            style={
+              cellValue.value == 0 ? 
+              {color: "red"} : null
+            }
+          >
+            {cellValue.value}
+          </div>
+        )
+      }  
     },
     {
       field: "total",
       headerName: "รวม",
       headerClassName: 'super-app-theme--header',
+      headerAlign: 'right',
       width: 120,
-      renderCell: function total(params) {
-        return params.row.type * params.row.qty;
-      },
+      type: "number",
+      renderCell: (cellValue) => {
+        return (
+          <div>
+            {cellValue.row.type * cellValue.row.qty}
+          </div>
+        )
+      } 
     },
-    // {
-    //   field: "action",
-    //   headerName: "action",
-    //   headerClassName: 'super-app-theme--header',
-    //   width: 120,
-    //   disableClickEventBubbling: true,
-    //   renderCell: function delete_row(params) {
-    //     if(params.row.action === undefined){
-    //       return (<Button
-    //       onClick={() => handleDelete(params)}
-    //       variant="contained"
-    //       color="secondary"
-    //     >
-    //       ลบ
-    //     </Button>)
-
-
-    //     }
-    //   },
-    // }
+    {
+      field: "",
+      headerName: "action",
+      headerClassName: 'super-app-theme--header',
+      width: 120,
+      disableClickEventBubbling: true,
+      renderCell: function delete_row(params) {
+          return (<Button
+          onClick={() => handleDelete(params)}
+          // variant="contained"
+          color="rose"
+        >
+          ลบ
+        </Button>)     
+      },
+    }
   ];
 
   useEffect(() => {
@@ -229,102 +218,103 @@ function GenerateCoupon({ customers, coupons }) {
   };
 
   
-
-
-  //List toggle component
-
-
-
-  
-
-
-
   const onSubmit = () => {
     let test = groupByKey(rows, "type")
 
-    Object.keys(test).some(type => test[type].length > 1) ? alert("กรุณาระบุราคาเดียวกันในช่องเดียวกัน") :
+    if(Object.keys(test).some(type => test[type].length > 1)){
+      alert("กรุณาระบุราคาเดียวกันในช่องเดียวกัน")
+
+    }else{
+      if(rows.some(row => Number(row.type) < 500 || (Number(row.type) % 100 !== 0 && Number(row.type) % 100 !== 50))){
+        alert("กรุณาระบุราคาคูปองไม่ต่ำกว่า 500 และไม่ควรมีเศษ")
+
+      }else{
+        rows.map((row) => {
+
+          if (Object.keys(ordered_company).includes(row.type.toString())) {
     
-    rows.some(row => Number(row.type) < 500 || (Number(row.type) % 100 !== 0 && Number(row.type) % 100 !== 50)) ? 
-
-    alert("กรุณาระบุราคาคูปองไม่ต่ำกว่า 500 และไม่ควรมีเศษ") : 
-
-    rows.map((row) => {
-
-      if (Object.keys(ordered_company).includes(row.type.toString())) {
-
-        let runNo = ordered_company[row.type.toString()].length;
-
-        for (let i = 1; i <= Number(row.qty); i++) {
-          fetch("/api/coupon", {
-            method: "POST", // *GET, POST, PUT, DELETE, etc.
-            mode: "cors", // no-cors, *cors, same-origin
-            cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-            credentials: "same-origin", // include, *same-origin, omit
-            headers: {
-              "Content-Type": "application/json",
-              // 'Content-Type': 'application/x-www-form-urlencoded',
-            },
-            redirect: "follow", // manual, *follow, error
-            referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-            body: JSON.stringify({
-              code:
-                company._id + "-" + date + "-" + row.type + "-" + (runNo + i),
-              companyRef: company._id,
-              generatedDate: date,
-              amount: Number(row.type),
-              runningNo: runNo + i,
-              used: false,
-              usedDateTime: "",
-              recordedBy: "",
-              printed: false,
-            }), // body data type must match "Content-Type" header
-          });
-          if (i == Number(row.qty)) {
-            alert("สร้างคูปอง " + row.type + " บาท สำเร็จ");
+            let runNo = ordered_company[row.type.toString()].length;
+    
+            for (let i = 1; i <= Number(row.qty); i++) {
+              fetch("/api/coupon", {
+                method: "POST", // *GET, POST, PUT, DELETE, etc.
+                mode: "cors", // no-cors, *cors, same-origin
+                cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+                credentials: "same-origin", // include, *same-origin, omit
+                headers: {
+                  "Content-Type": "application/json",
+                  // 'Content-Type': 'application/x-www-form-urlencoded',
+                },
+                redirect: "follow", // manual, *follow, error
+                referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+                body: JSON.stringify({
+                  code:
+                    company._id + "-" + date + "-" + row.type + "-" + (runNo + i),
+                  companyRef: company._id,
+                  generatedDate: date,
+                  amount: Number(row.type),
+                  runningNo: runNo + i,
+                  used: false,
+                  usedDateTime: "",
+                  recordedBy: "",
+                  printed: false,
+                }), // body data type must match "Content-Type" header
+              });
+              if (i == Number(row.qty)) {
+                alert("สร้างคูปอง " + row.type + " บาท สำเร็จ");
+              }
+            }
+          } else {
+            
+            for (let i = 1; i <= Number(row.qty); i++) {
+              fetch("/api/coupon", {
+                method: "POST", // *GET, POST, PUT, DELETE, etc.
+                mode: "cors", // no-cors, *cors, same-origin
+                cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+                credentials: "same-origin", // include, *same-origin, omit
+                headers: {
+                  "Content-Type": "application/json",
+                  // 'Content-Type': 'application/x-www-form-urlencoded',
+                },
+                redirect: "follow", // manual, *follow, error
+                referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+                body: JSON.stringify({
+                  code: company._id + "-" + date + "-" + row.type + "-" + i,
+                  companyRef: company._id,
+                  generatedDate: date,
+                  amount: Number(row.type),
+                  runningNo: i,
+                  used: false,
+                  usedDateTime: "",
+                  recordedBy: "",
+                  printed: false,
+                }), // body data type must match "Content-Type" header
+              });
+              if (i == Number(row.qty)) {
+                alert("สร้างคูปอง " + row.type + " บาท สำเร็จ");
+              }
+            }
           }
-        }
-      } else {
+        })
         
-        for (let i = 1; i <= Number(row.qty); i++) {
-          fetch("/api/coupon", {
-            method: "POST", // *GET, POST, PUT, DELETE, etc.
-            mode: "cors", // no-cors, *cors, same-origin
-            cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-            credentials: "same-origin", // include, *same-origin, omit
-            headers: {
-              "Content-Type": "application/json",
-              // 'Content-Type': 'application/x-www-form-urlencoded',
-            },
-            redirect: "follow", // manual, *follow, error
-            referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-            body: JSON.stringify({
-              code: company._id + "-" + date + "-" + row.type + "-" + i,
-              companyRef: company._id,
-              generatedDate: date,
-              amount: Number(row.type),
-              runningNo: i,
-              used: false,
-              usedDateTime: "",
-              recordedBy: "",
-              printed: false,
-            }), // body data type must match "Content-Type" header
-          });
-          if (i == Number(row.qty)) {
-            alert("สร้างคูปอง " + row.type + " บาท สำเร็จ");
-          }
-        }
+        router.reload()
       }
-    })
-    
-    router.reload()    
+
+    }
+
+        
   };
 
 
   return (
     <div>
       
-            <GridContainer className={classes2.formControl}>
-              <FormControl variant="outlined" className={classes2.formControl}>
+            <GridContainer 
+            // className={classes2.formControl}
+            >
+              <FormControl 
+              variant="outlined" className={classes2.formControl}
+              >
                 <InputLabel id="demo-simple-select-outlined-label1">
                   ชื่อลูกค้า
                 </InputLabel>
@@ -345,8 +335,10 @@ function GenerateCoupon({ customers, coupons }) {
                 </Select>
               </FormControl>
 
-              <FormControl variant="outlined" className={classes2.formControl}>
-                <Button href="/coupon/printPage" color="primary">
+              <FormControl 
+              variant="outlined" className={classes2.formControl}
+              >
+                <Button href="/coupon/printPage" color="info">
                   หน้าปริ้น
                 </Button>
               </FormControl>
@@ -359,7 +351,7 @@ function GenerateCoupon({ customers, coupons }) {
               {tableState ? (
                 <div>
                   
-                <div style={{ height: 300, width: "92%" }} className={classes4.root}>
+                <div style={{ height: 300, width: "120%" }} className={classes4.root}>
                 <DataGrid
                   rows={rows}
                   columns={columns}
@@ -369,10 +361,10 @@ function GenerateCoupon({ customers, coupons }) {
                   />
                   </div>
             <div>
-              <Button onClick={() => handleAddRow()}>เพิ่มคูปอง</Button>
+              <Button color="primary" onClick={() => handleAddRow()}>เพิ่มคูปอง</Button>
               &emsp;คูปองที่ต้องการพิมพ์ทั้งหมด มูลค่ารวม {totalCoupon}
             </div>
-            <Button onClick={() => onSubmit()} color="primary">
+            <Button onClick={() => onSubmit()} color="success">
               ยืนยัน
             </Button>
             &emsp;
