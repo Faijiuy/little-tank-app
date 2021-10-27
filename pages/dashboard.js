e7f72456c4f3d3cfb64b019da3c2e74588cb0090
patlim8 <patsap8@gmail.com>
2021-10-27 20:35:56 +0700

    thousand seperator

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -270,6 +270,12 @@ function groupByKeys_customer_remain(arr) {
   return result
 } 
 
+function thousands_separators(num) {
+  var num_parts = num.toString().split(".");
+  num_parts[0] = num_parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  return num_parts.join(".");
+}
+
 function Dashboard({ customer: customers, coupon: coupons }) {
   const classes = useStyles()
   const [sortCoupons, setSortCoupons] = useState([])
@@ -313,6 +319,9 @@ const oldest_coupon_group_columns = [
     headerClassName: "super-app-theme--header",
     headerName: "คูปอง",
     width: 120,
+    renderCell: function render_number(params){
+      return thousands_separators(Number(params.row.amount))
+    }
   //   editable: true,
   },
   {
@@ -349,26 +358,22 @@ const customer_remain_columns = [
     headerClassName: "super-app-theme--header",
     headerName: "คูปอง",
     width: 120,
+    renderCell: function render_number(params){
+      return thousands_separators(Number(params.row.amount))
+    }
   //   editable: true,
   },
 ];
 
   useEffect(() => {
-
-    // let coupons = []
     setLabelDate(getLabelDate())
     let used_coupons = coupons.filter(coupon => coupon.used === true)
     let unused_coupons = coupons.filter(coupon => coupon.used === false)
-
-    // console.log(used_coupons)
 
     let sort_coupon_byDate = getDaysAgoData(used_coupons, 30)
 
     let group_by_companyRef = groupByKey(sort_coupon_byDate, 'companyRef')
 
-    // console.log('group_by_companyRef: ', group_by_companyRef)
-
-    // console.log(fill_date_gaps(group_by_companyRef))
     setDatasets(fill_date_gaps(group_by_companyRef, customers))
 
     setOldest_coupon_group(groupByKeys(coupons))
@@ -380,75 +385,12 @@ const customer_remain_columns = [
     }, 0)
 
     setTotalamount(total)
-    console.log(total)
 
   }, [])
 
 
-
-  // useEffect(() => {
-
-  //   const sdk = new ChartsEmbedSDK({
-  //     baseUrl: "https://charts.mongodb.com/charts-little-tank-jnblp",
-  //   });
-  //   const chart = sdk.createChart({
-  //     chartId: "b91605a3-b18c-4782-a755-bb0d6ae0cc27",
-  //     width: 600,
-  //     height: 220,
-  //     theme: "dark",
-  //   });
-
-  //   const couponsRemain = sdk.createChart({
-  //     chartId: "eea67d55-e0a7-4cc7-9eba-77cc25a48a7b",
-  //     width: 630,
-  //     height: 380,
-  //     theme: "dark",
-  //   });
-
-  //   const lineChart = sdk.createChart({
-  //     chartId: "4c2a3639-147b-4532-b6a0-321ab9eec6fb",
-  //     width: 600,
-  //     height: 390,
-  //     theme: "dark",
-  //   });
-
-  //   const oldestCoupon = sdk.createChart({
-  //     chartId: "9188d9fb-81e8-49f2-b939-d724888136e7",
-  //     width: 630,
-  //     height: 390,
-  //     theme: "dark",
-  //   });
-
-  //   chart
-  //     .render(document.getElementById("chart"))
-  //     .catch(() => window.alert("Chart failed to initialise"));
-
-  //   couponsRemain
-  //     .render(document.getElementById("chart2"))
-  //     .catch(() => window.alert("Chart failed to initialise"));
-
-  //   lineChart
-  //     .render(document.getElementById("chart3"))
-  //     .catch(() => window.alert("Chart failed to initialise"));
-
-  //   oldestCoupon
-  //     .render(document.getElementById("chart4"))
-  //     .catch(() => window.alert("Chart failed to initialise"));
-
-  // }, []);
-
   return (
-    <GridContainer style={{margin: 10}}>
-      {/* <span>
-        <Box className="left" id="chart3" />
-        <Box className="left" id="chart4" />
-      </span>
-      <span>
-        <Box className="left" id="chart" />
-        <Box className="left" id="chart2" />
-      </span> */}
-      {/* test */}
-
+    <GridContainer>
       <Box className={classes.paper_lineChart}>
         <p style={{textDecoration: 'underline'}}>ยอดการใช้คูปองรายวัน</p>
         <Line
@@ -464,7 +406,7 @@ const customer_remain_columns = [
 
       <Box className={classes.paper_totalAmount}>
         <p style={{textDecoration: 'underline'}}>ยอดเงินคูปองที่ยังไม่ได้ใช้ทั้งหมด</p>
-        <h1>{totalamount}</h1>
+        <h1>{thousands_separators(totalamount)}</h1>
       </Box>
 
           
